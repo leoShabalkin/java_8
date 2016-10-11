f430b6ea7ddbab01ad81754477dcf6a8faa26c18
Leonid_Shabalkin <Leonid_Shabalkin@epam.com>
2016-10-11 19:03:41 +0300

    java_8. Добавление static метода

--- src/main/java/functional/CalcInterface.java
+++ src/main/java/functional/CalcInterface.java
@@ -8,6 +8,13 @@ import java.math.BigDecimal;
 @FunctionalInterface
 public interface CalcInterface extends MathInterface {
    default BigDecimal calc(BigDecimal value1, BigDecimal value2) {
+       if (!isNull(value1)){
+           return BigDecimal.ZERO;
+       }
        return value1.multiply(value2);
    }
+    static boolean isNull(BigDecimal value) {
+        System.out.println("Interface Null Check");
+        return value == null || value.compareTo(BigDecimal.ZERO) == 0;
+    }
 }
